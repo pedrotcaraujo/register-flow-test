8794664efce595b03a5bcf44961a9def3fd97244
Pedro Araujo <pedrotcaraujo@gmail.com>
2017-06-19 02:58:29 -0300

    scroll e mensagens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,16 @@
 (function() {
-	var $typing = $('.typing');
 	var scope = {}
-	var initialHeigth = $('.messages').innerHeight();
-
-	function checkScroll() {
-		console.log($('.messages').innerHeight(), innerHeight);
-		if($('.messages').height > initialHeigth) {
-			$('.messages').show();
-		}
-	}
-
 
 	function typemessage(messages, index) {
-		$typing.show();
+		$(messages.get(index)).addClass('loading');
+		$(messages.get(index)).fadeIn('slow', function() {
+			$('.messages').scrollTop($('.messages')[0].scrollHeight);
+		});
 		setTimeout(function() {
-			$(messages.get(index)).fadeIn();
-			$('.messages').scrollTop(500);
-			$typing.hide();
+			$(messages.get(index)).removeClass('loading');
+			$(messages.get(index)).find('.text').fadeIn('slow', function() {
+				$('.messages').scrollTop($('.messages')[0].scrollHeight);
+			});
 			if(messages.get(++index)) {
 				typemessage(messages, index)
 			}
@@ -40,8 +34,8 @@
 	$(document).on('submit', '#greetings-form form', function(e) {
 		e.preventDefault();
 		$this = $(this)
-		var userName = $this.find('input').val()
-		scope['userName'] = userName;
+		var value = $this.find('input').val()
+		scope['userName'] = value;
 		$('#greetings').next().find('p').fadeIn()
 		bindScopes('userName')
 		nextStep('#documentations')
@@ -51,10 +45,100 @@
 	$(document).on('submit', '#documentations-form form', function(e) {
 		e.preventDefault();
 		$this = $(this)
-		var document = $this.find('input').val()
-		scope['document'] = document;
+		var value = $this.find('input').val()
+		scope['document'] = value;
 		$('#documentations').next().find('p').fadeIn()
 		bindScopes('document')
 		nextStep('#address-cep')
+	})
+
+	$(document).on('submit', '#address-cep-form form', function(e) {
+		e.preventDefault();
+		$this = $(this)
+		var value = $this.find('input').val()
+		scope['cep'] = value;
+		$('#address-cep').next().find('p').fadeIn()
+		bindScopes('cep')
+		nextStep('#address-cep-confirm')
+	})
+
+	$(document).on('submit', '#address-cep-confirm-form form', function(e) {
+		e.preventDefault();
+		$this = $(this)
+		var value = $this.find('input').val()
+		scope['cepConfirm'] = value;
+		$('#address-cep-confirm').next().find('p').fadeIn()
+		bindScopes('cepConfirm')
+		nextStep('#address-additional')
+	})
+
+	$(document).on('submit', '#address-additional-form form', function(e) {
+		e.preventDefault();
+		$this = $(this)
+		var value = $this.find('input').val()
+		scope['address'] = value;
+		$('#address-additional').next().find('p').fadeIn()
+		bindScopes('address')
+		nextStep('#email')
+	})
+
+	$(document).on('submit', '#email-form form', function(e) {
+		e.preventDefault();
+		$this = $(this)
+		var value = $this.find('input').val()
+		scope['email'] = value;
+		$('#email').next().find('p').fadeIn()
+		bindScopes('email')
+		nextStep('#email-confirm')
+	})
+
+	$(document).on('submit', '#email-confirm-form form', function(e) {
+		e.preventDefault();
+		$this = $(this)
+		var value = $this.find('input').val()
+		scope['emailConfirm'] = value;
+		$('#email-confirm').next().find('p').fadeIn()
+		bindScopes('emailConfirm')
+		nextStep('#phone')
+	})
+
+	$(document).on('submit', '#phone-form form', function(e) {
+		e.preventDefault();
+		$this = $(this)
+		var value = $this.find('input').val()
+		scope['phone'] = value;
+		$('#phone').next().find('p').fadeIn()
+		bindScopes('phone')
+		nextStep('#payment-method')
+	})
+
+	$(document).on('submit', '#payment-method-form form', function(e) {
+		e.preventDefault();
+		$this = $(this)
+		var value = $this.find('input').val()
+		scope['paymentMethod'] = value;
+		$('#payment-method').next().find('p').fadeIn()
+		bindScopes('paymentMethod')
+		nextStep('#payment-register')
+	})
+
+	$(document).on('submit', '#payment-register-form form', function(e) {
+		e.preventDefault();
+		$this = $(this)
+		var value = $this.find('input').val()
+		scope['creditCard'] = value;
+		$('#payment-register').next().find('p').fadeIn()
+		bindScopes('creditCard')
+		nextStep('#pick-address')
+	})
+
+	$(document).on('submit', '#pick-address-form form', function(e) {
+		e.preventDefault();
+		$this = $(this)
+		var value = $this.find('input').val()
+		scope['deliveryAddress'] = value;
+		$('#pick-address').next().find('p').fadeIn()
+		bindScopes('deliveryAddress')
+		nextStep('#done')
 	})
 })()
